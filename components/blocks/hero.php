2f76a233fd192378fbc68a5fc8fe7425da5561a1
ngonzales07 <Admin@N-Gonzales-MacBook-Pro.local>
2020-02-11 18:40:09 -0800

    initial setup and hero block

---
+++ components/blocks/hero.php
@@ -0,0 +1,11 @@
+<?php
+  $content = get_sub_field('content');
+?>
+
+<div class="row block__hero">
+  <div class="col-xl-6 offset-xl-1">
+    <div class="column-content">
+      <?php echo $content; ?>
+    </div>
+  </div>
+</div>
--- components/site-nav.php
+++ components/site-nav.php
@@ -1,18 +1,18 @@
 <nav class="site-nav">
-	<a class="logo" href="<?php echo esc_url(home_url('/')); ?>">
-		<?php get_template_part('components/svg/logo'); ?>
-	</a>
+    <a class="logo" href="<?php echo esc_url(home_url('/')); ?>">
+        <?php get_template_part('components/svg/logo'); ?>
+    </a>
 
-	<?php $args = array(
-		'theme_location'  => 'header-menu',
-		'container'       => false,
-		'menu_class'      => 'menu'
-	);
-	wp_nav_menu($args); ?>
+    <?php $args = array(
+    'theme_location'  => 'header-menu',
+    'container'       => false,
+    'menu_class'      => 'menu'
+    );
+    wp_nav_menu($args); ?>
 
-	<?php get_template_part('components/call'); ?>
-	<?php get_template_part('components/social-icons'); ?>
-	<button data-toggle="menu">
+    <?php get_template_part('components/social-icons'); ?>
+    <?php get_template_part('components/call'); ?>
+    <button data-toggle="menu">
         <span></span>
         <span></span>
         <span></span>
